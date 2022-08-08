cf75a08f17150df8271e07c79927c3dc63017267
Adrienne Davis <adrienne@adriennealyse.com>
2022-08-08 17:36:07 -0500

    Made search function dynamic.

--- src/App.js
+++ src/App.js
@@ -9,18 +9,19 @@ import MovieCard from './MovieCard';
 
 const API_URL = ' http://www.omdbapi.com/?apikey=91a77b30'
 
-const movie1 = {
-  "Title": "Spiderman in Cannes",
-  "Year": "2016",
-  "imdbID": "tt5978586",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
-};
+// const movie1 = {
+//   "Title": "Spiderman in Cannes",
+//   "Year": "2016",
+//   "imdbID": "tt5978586",
+//   "Type": "movie",
+//   "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
+// };
 
 
 const App = () => {
 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -39,18 +40,25 @@ const App = () => {
 
       <div className='search'>
         <input placeholder='Search for movies' 
-          value='Superman' 
-          onChange={() => {}}/>
+          value={searchTerm} 
+          onChange={(e) => setSearchTerm(e.target.value)}/>
 
-        <img src={SearchIcon} alt='search' onClick={() => {}}/>
+        <img src={SearchIcon} alt='search' onClick={() => searchMovies(searchTerm)}/>
       </div>
 
-      
-
-      <div className='container'>
-        <MovieCard movie1={movies[0]}/>
-      </div>
-
+      {movies?.length > 0
+          ? (
+            <div className='container'>
+              {movies.map((movie) => (
+                <MovieCard movie={movie}/>
+              ))}
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>No movies found</h2>
+            </div>
+          )
+      }
     </div>  
   );
 }
